e19dca51b491338d7b3538c5970d34507ce6252f
joesu-angible <joesu@angible.com>
2025-01-17 12:48:59 +0800

    Update cvat_init.sh

--- cvat_init.sh
+++ cvat_init.sh
@@ -4,4 +4,6 @@ sudo mkdir -p /data/cvat/cvat_logs
 sudo mkdir -p /data/cvat/cvat_events_db
 sudo mkdir -p /data/cvat/cvat_cache_db
 sudo mkdir -p /data/cvat/cvat_share
+sudo mkdir -p /data/cvat/cvat_db
+sudo mkdir -p /data/cvat/cvat_inmem_db
 sudo chmod 777 -R /data/cvat
